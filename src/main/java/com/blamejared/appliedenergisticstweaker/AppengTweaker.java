77ce226135434db3c2918349223ec0d4de89b6e1
Jared <jaredlll08@gmail.com>
2021-02-27 22:06:55 +0200

    files

---
+++ src/main/java/com/blamejared/appliedenergisticstweaker/AppengTweaker.java
@@ -0,0 +1,11 @@
+package com.blamejared.appliedenergisticstweaker;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod("appliedenergisticstweaker")
+public class AppengTweaker {
+    public AppengTweaker() {
+    
+    }
+    
+}
